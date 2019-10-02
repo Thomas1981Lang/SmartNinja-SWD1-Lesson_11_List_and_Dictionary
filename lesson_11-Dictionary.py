2c3e834f5b2d4785a011b226de0c103d73861b3b
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2019-10-02 16:49:15 +0200

    Add Dictionary Basics

---
+++ lesson_11-Dictionary.py
@@ -0,0 +1,44 @@
+# Dictionary
+
+print('List')
+box = [20, 45, 30]
+print(box)
+print(type(box))
+
+print('')
+print('')
+
+print('Dictionary')
+box = {
+    'height': 20,
+    'width': 45,
+    'length': 30
+}
+print(box)
+print(type(box))
+
+print('Access the height of the box')
+print(box['height'])
+print(box.get('height'))
+
+print('')
+print('')
+
+print('List Example')
+eu_cities = ['Vienna', 'Malaga', 'Budapest', 'Cologne', 'Zagreb']
+print(eu_cities)
+print(type(eu_cities))
+
+print('')
+print('')
+
+print('Dictionary Example')
+jordan = {
+    'first_name': 'Michael',
+    'last_name': 'Jordan',
+    'age': 56,
+    'games_played': 1072,
+    'total_points': 32292
+}
+print(jordan)
+print(type(jordan))
